91445aeb91a87bb684cbbfb9feab80972940aec8
Eric Menne <info@eric-menne.com>
2025-01-31 00:32:13 +0100

    Add instructions attributes

--- src/commands.rs
+++ src/commands.rs
@@ -77,3 +77,65 @@ pub enum Instruction {
     BRC = 10,
     BRN = 11,
 }
+
+pub fn get_instruction_attribute(str: &str) -> Option<InstructionAttribute> {
+    Some(match str.to_uppercase().as_str() {
+        "NOP" => InstructionAttribute::from(0b00000000),
+        "LDA" => InstructionAttribute::from(0b00000011),
+        "STA" => InstructionAttribute::from(0b00000010),
+        "ADD" => InstructionAttribute::from(0b00000011),
+        "SUB" => InstructionAttribute::from(0b00000011),
+        "JMP" => InstructionAttribute::from(0b00000101),
+        "BRZ" => InstructionAttribute::from(0b00000001),
+        "BRC" => InstructionAttribute::from(0b00000001),
+        "BRN" => InstructionAttribute::from(0b00000001),
+        _ => return None,
+    })
+}
+
+/// ```plaintext
+/// +-------------------------+
+/// |    0 b 0 0 0 0 0 0 0 0  |
+/// |                  ↑ ↑ ↑  |
+/// |                  | | |  |
+/// |       Label    <━┛ | |  |
+/// |       Address    <━┛ |  |
+/// |       Fixed number <━┛  |
+/// +-------------------------+
+/// ```
+///
+/// Bit Meaning:
+/// - Bit 0 (1st bit): Fixed numbers are allowed (0b00000001)
+/// - Bit 1 (2nd bit): Addresses are allowed (0b00000010)
+/// - Bit 2 (3rd bit): Labels are allowed (0b00000100)
+/// - Bit 3 (4th bit): Don't care
+/// - Bit 4 (5th bit): Don't care
+/// - Bit 5 (6th bit): Don't care
+/// - Bit 6 (7th bit): Don't care
+/// - Bit 7 (8th bit): Don't care
+///
+pub(crate) struct InstructionAttribute(u8);
+
+impl From<u8> for InstructionAttribute {
+    fn from(value: u8) -> Self {
+        Self(value)
+    }
+}
+
+impl InstructionAttribute {
+    pub fn allow_no_operant(&self) -> bool {
+        self.0 == 0b00000000
+    }
+    
+    pub fn allow_fixed_number(&self) -> bool {
+        (self.0 & 0b00000001) != 0
+    }
+
+    pub fn allow_address(&self) -> bool {
+        (self.0 & 0b00000010) != 0
+    }
+
+    pub fn allow_label(&self) -> bool {
+        (self.0 & 0b00000100) != 0
+    }
+}
